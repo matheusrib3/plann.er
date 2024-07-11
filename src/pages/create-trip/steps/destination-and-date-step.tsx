2dbb70d672b8e8532777533a52c3488c2598a380
Matheus Ribeiro <matheushenribeiro@outlook.com>
2024-07-11 17:25:03 -0300

    new file:   .gitignore 	modified:   node_modules/.package-lock.json 	modified:   node_modules/.vite/deps/_metadata.json 	deleted:    node_modules/.vite/deps/chunk-6BKLQ22S.js 	deleted:    node_modules/.vite/deps/chunk-6BKLQ22S.js.map 	deleted:    node_modules/.vite/deps/chunk-DRWLMN53.js 	deleted:    node_modules/.vite/deps/chunk-DRWLMN53.js.map 	deleted:    node_modules/.vite/deps/chunk-G3PMV62Z.js 	deleted:    node_modules/.vite/deps/chunk-G3PMV62Z.js.map 	modified:   node_modules/.vite/deps/lucide-react.js 	modified:   node_modules/.vite/deps/react-dom_client.js 	modified:   node_modules/.vite/deps/react-router-dom.js 	modified:   node_modules/.vite/deps/react.js 	modified:   node_modules/.vite/deps/react_jsx-dev-runtime.js 	modified:   node_modules/.vite/deps/react_jsx-runtime.js 	modified:   node_modules/.vite/deps/react_jsx-runtime.js.map 	modified:   node_modules/.vite/deps/tailwind-variants.js 	modified:   package-lock.json 	modified:   package.json 	modified:   src/index.css 	modified:   src/pages/create-trip/steps/destination-and-date-step.tsx

--- src/pages/create-trip/steps/destination-and-date-step.tsx
+++ src/pages/create-trip/steps/destination-and-date-step.tsx
@@ -1,5 +1,10 @@
-import { ArrowRight, Calendar, MapPin, Settings2 } from "lucide-react";
+import { ArrowRight, Calendar, MapPin, Settings2, X } from "lucide-react";
 import { Button } from "../../../components/button";
+import { useState } from "react";
+import { DateRange, DayPicker } from "react-day-picker";
+import "react-day-picker/dist/style.css";
+import {format } from 'date-fns'
+
 
 interface DestinationAndDateStepProps {
 
@@ -9,6 +14,20 @@ interface DestinationAndDateStepProps {
 }
 
 export function DestinationAndDateStep({ isGuestsInputOpen, openGuestsInput, closeGuestsInput }: DestinationAndDateStepProps) {
+    const [isDatePickerOpen, setIsDatePickerOpen] = useState(false)
+    const [eventStartAndEnDates, setEventStartAndEndDates] = useState<DateRange | undefined>()
+    
+
+    function openDatePicker() {
+        return setIsDatePickerOpen(true)
+    }
+
+    function closeDatePicker() {
+        return setIsDatePickerOpen(false)
+    }
+
+    const displayDate = eventStartAndEnDates  && eventStartAndEnDates.from  && eventStartAndEnDates.to ? 
+    format(eventStartAndEnDates.from, "d' de 'LLL").concat(' até ').concat(format(eventStartAndEnDates.to, "d' de 'LLL")) : null
 
     return (
         <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
@@ -17,10 +36,36 @@ export function DestinationAndDateStep({ isGuestsInputOpen, openGuestsInput, clo
                 <input disabled={isGuestsInputOpen} type="text" placeholder="Para onde você vai?" className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1" />
             </div>
 
-            <div className='flex items-center gap-2'>
+            <button onClick={openDatePicker} disabled={isGuestsInputOpen} className='flex items-center gap-2 text-left w-[240px]'>
                 <Calendar className="size-5 text-zinc-400" />
-                <input disabled={isGuestsInputOpen} type="text" placeholder="Quando?" className="bg-transparent text-lg placeholder-zinc-400 w-40 outline-none" />
-            </div>
+                <span className=" text-lg text-zinc-400 w-40 flex-1">{displayDate || 'Quando?'}
+                </span>
+                
+            </button>
+
+            {isDatePickerOpen && (
+                <div className='fixed inset-0 bg-black/60 flex items-center justify-center'>
+
+                    <div className=' rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5'>
+
+                        <div className='space-y-2'>
+                            <div className='flex items-center justify-between'>
+
+                                <h2 className='text-lg font-semibold'>Selecione a data</h2>
+                                <button type="button" onClick={closeDatePicker}>
+                                    <X className="size-5 text-zinc-400" />
+                                </button>
+
+                            </div>
+                        </div>
+
+                        <DayPicker mode="range" selected={eventStartAndEnDates} onSelect={setEventStartAndEndDates} />
+
+                    </div>
+
+                </div>)
+
+            }
 
             <div className='w-px h-6 bg-zinc-800'></div>
 
@@ -32,8 +77,6 @@ export function DestinationAndDateStep({ isGuestsInputOpen, openGuestsInput, clo
                 </Button>
 
             ) : (
-
-
                 <Button onClick={openGuestsInput} variant="primary" size="default">
                     Continuar
                     <ArrowRight className="size-5"></ArrowRight>
